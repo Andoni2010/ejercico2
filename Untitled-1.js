2a2562ad59b0fce5a73789e623e5fdcf18e62214
Andoni2010 <98291437+Andoni2010@users.noreply.github.com>
2022-07-09 14:25:01 +0200

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,18 @@
+
+//Si soypogramador
+const Soy_Desarrollador = true;
+
+//Nombre y años
+const nombre_anyo = ["Andoni", 22];
+//Fecha nacimiento
+const dia_mes_anyo = [03, 10, 1999];
+//Libro
+const Libro = {
+    titulo: "hey",
+    autor: "Saludos",
+    fecha: "Del norte",
+    url: "https://andoni2010.github.io/portafoliosAndoni/"
+};
+const ejercicio = [nombre_anyo, Soy_Desarrollador, dia_mes_anyo, Libro];
+ //error e.console.log.apply is not a function 
+console.log(ejercicio);
